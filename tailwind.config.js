56eda52679eec81d836019f6c4f30692ee9cbae5
RomanHrabovyi <romansudova@gmail.com>
2024-07-19 15:42:18 +0300

    Hello Elizabeth

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './template/customers/*.liquid',
+    './tamplate/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
